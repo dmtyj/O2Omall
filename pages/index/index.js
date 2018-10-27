d76d7acb089ed8655fc3406d65a90a9e6b370b11
DuQiadi <13011725325@qq.com>
2018-10-27 16:00:14 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,19 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World wechat',
+    userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+ 
+  }
+})
---
+++ pages/store/history/payin/payin.js
@@ -0,0 +1,29 @@
+Page({
+  data: {
+  },
+
+  onLoad: function (options) {
+    console.log("onLoad")
+  },
+  onReady: function () {
+    console.log("onReady")
+  },
+  onShow: function () {
+    console.log("onShow")
+  },
+  onHide: function () {
+    console.log("onHide")
+  },
+  onUnload: function () {
+    console.log("onUnload")
+  },
+  onPullDownRefresh: function () {
+    console.log("onPullDownRefresh")
+  },
+  onReachBottom: function () {
+    console.log("onReachBottom")
+  },
+  onShareAppMessage: function () {
+    console.log("onShareAppMessage")
+  }
+})
